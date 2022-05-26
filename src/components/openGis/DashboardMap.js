6b78953dae2551d0f7a770916ad118c54e50b76e
phelixdusengimana <phelixdusengimana@gmail.com>
2022-05-26 19:02:51 +0200

    feat: moved dialog to its single component.

--- src/components/openGis/DashboardMap.js
+++ src/components/openGis/DashboardMap.js
@@ -54,6 +54,7 @@ import SearchData from '../../utils/Search';
 import { SearchNotEqual, SearchEqual } from '../../utils/EqualAndNotEqual'
 import './Table.css';
 import { funcContext } from '../../GlobalState'
+import MapDialogBox from "./MapDialogBox";
 
 const DashboardMap = (props) => {
   console.log(props, " ---------------> props in dashbaord")
@@ -77,12 +78,6 @@ const DashboardMap = (props) => {
   const [context, setContext] = useContext(funcContext)
   console.log(context, " ------------- context")
 
-  // select options 
-  const mapFilterOptions = [
-    { value: 'poles', label: 'Poles' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ]
   // useEffect(() => {
   //   const runAsync = async () => {
   //     const res = await getLayersByRoleId(roleId);
@@ -575,68 +570,15 @@ const DashboardMap = (props) => {
   };
 
   const handleFilterModel = ()=>{
-    setmapFilter(!mapFilter)
-  }
-
-  const handleFilterMapSubmit = (event) => {
-    event.preventDefault();
+     setmapFilter(!mapFilter)
   }
 
   return (
     <>
 
-<dialog open={mapFilter} className="dialog-box">
-    <header className="header">
-      <h1>Map Filter</h1>
-      <button onClick={()=>{handleFilterModel()}} className="icon"><i className="fas fa-times"></i></button>
-    </header>
-    <form role={"contentinfo"} onSubmit={(e)=>{handleFilterMapSubmit(e)}}>
-     <div className="input_container">
-      <label>Layer</label>
-      {/* <Select options={mapFilterOptions} /> */}
-      <select name="selection" className="selection"> 
-        {mapFilterOptions.map((el)=>(
-          <option value={el.value}>{el.label}</option>
-        ))}
-      </select>
-     </div>
-     <div className="input_container flex">
-        <div>
-          <label>Property</label>
-          <select name="property">
-            <option value={""} disabled selected>Height</option>
-          </select>
-        </div>
-        <div>
-          <label>Condition</label>
-        <select name="condition">
-        <option value={""} disabled selected>Greater than</option>
-        </select>
-        </div>
-        <div>
-          <label>Value</label>
-        <select name="value">
-        <option value={""} disabled selected>15</option>
-        </select>
-        </div>
-     </div>
-     <div className="input_container option_button">
-        <button className="add">Add</button>
-        <button className="remove">Remove</button>
-     </div>
-
-     <div className="input_container">
-       <input name="features"></input>
-       <p className="text-center mt-4">34 features selected</p>
-     </div>
-     <footer>
-       <div className="input_container  option_button">
-            <button className="clear">Clear</button>
-            <button className="preview">Preview</button>
-       </div>
-     </footer>
-    </form>
-</dialog>
+      <dialog open={mapFilter} className="dialog-box">
+       <MapDialogBox handleFilterModel={handleFilterModel}/>
+      </dialog>
 
       <div ref={mapElement} id="mapElement" className="map-container"></div>
       {/* <div
